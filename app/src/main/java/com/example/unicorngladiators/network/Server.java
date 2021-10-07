1fa71d4bb2ec0858df7670b6c474034967a6623f
Juwon Lee <juwon.lee@u.yale-nus.edu.sg>
2021-10-07 17:01:31 +0800

    server added

--- app/src/main/java/com/example/unicorngladiators/network/Server.java
+++ app/src/main/java/com/example/unicorngladiators/network/Server.java
@@ -5,6 +5,17 @@ import java.net.*;
 public class Server {
 
     public static void main(String[] args) {
+        try{
+            ServerSocket ss = new ServerSocket(6666);
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            String str=(String)dis.readUTF();
+            System.out.println("Client Says = "+str);
+            ss.close();
+        }
 
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 }
