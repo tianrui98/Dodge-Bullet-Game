26cf5f319d72c93e3912a3f9d73734ff5b3d5420
Sewen Thy <sewenthy@gmail.com>
2021-10-16 12:14:39 +0800

    fixed mainactivity conflict

--- app/src/main/java/com/example/unicorngladiators/MainActivity.java
+++ app/src/main/java/com/example/unicorngladiators/MainActivity.java
@@ -3,10 +3,6 @@ package com.example.unicorngladiators;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import android.util.DisplayMetrics;
-import android.util.Log;
-
 import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.Button;
