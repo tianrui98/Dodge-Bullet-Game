d0dbea3adcf8b271098f8dbbb90b41ace6627a68
Tian Rui <zhutianrui1998@gmail.com>
2021-10-16 09:59:58 +0800

    added inputhandler and listener

---
+++ app/src/main/java/com/example/unicorngladiators/io/InputHandler.java
@@ -0,0 +1,17 @@
+package com.example.unicorngladiators.io;
+
+//determines what to do when user touches the joystick
+
+import com.example.unicorngladiators.model.Position;
+
+public class InputHandler<ClickAction> implements InputListener.Callback
+{
+    private ClickAction onClickAction;
+    public void setOnClickAction(ClickAction onClickAction) {
+        this.onClickAction = onClickAction;
+    }
+    @Override
+    public void onClick(Position pos) {
+//        if (onClickAction != null) onClickAction.execute(pos);
+    }
+}
--- app/src/main/java/com/example/unicorngladiators/io/InputListener.java
+++ app/src/main/java/com/example/unicorngladiators/io/InputListener.java
@@ -1,6 +1,32 @@
 package com.example.unicorngladiators.io;
 
-public class InputListener {
 
-    public interface Callback {}
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.unicorngladiators.model.Position;
+
+public class InputListener implements View.OnTouchListener {
+    private Position pos;
+    private Callback callback;
+    public void setCallback(InputHandler cb) {
+        this.callback = (Callback) cb;
+    }
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch(motionEvent.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_UP:
+                //TODO do we need to round it up?
+                pos = new Position(Math.round(motionEvent.getX()), Math.round(motionEvent.getY()));
+                callback.onClick(pos);
+                break;
+        }
+        return true;
+    }
+    public interface Callback {
+        void onClick ( Position pos ) ;
+    }
 }
