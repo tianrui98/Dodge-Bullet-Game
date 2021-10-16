de31f2e6cbef6ae245e2630abda6b7580e7866f0
Koa Zhao Yuan <claydol345@hotmail.com>
2021-10-16 12:18:07 +0800

    position tests done

--- app/src/test/java/com/example/unicorngladiators/ExampleUnitTest.java
+++ app/src/test/java/com/example/unicorngladiators/ExampleUnitTest.java
@@ -22,14 +22,13 @@ public class ExampleUnitTest {
     public void PositionAddTest() {
         for (int i = 0; i < 10; i++) {
             System.out.println(1);
-            int pos1X = (int) Math.random() * 20;
-            int pos1Y = (int) Math.random() * 20;
-            int pos2X = (int) Math.random() * 20;
-            int pos2Y = (int) Math.random() * 20;
+            int pos1X = (int) (Math.random() * 20);
+            int pos1Y = (int) (Math.random() * 20);
+            int pos2X = (int) (Math.random() * 20);
+            int pos2Y = (int) (Math.random() * 20);
             Position pos1 = new Position(pos1X, pos1Y);
             Position pos2 = new Position(pos2X, pos2Y);
             pos1.add(pos2);
-
             assertEquals(pos1.getX(), pos1X + pos2X);
             assertEquals(pos1.getY(), pos1Y + pos2Y);
         }
@@ -38,10 +37,10 @@ public class ExampleUnitTest {
     @Test
     public void PositionSubtractTest() {
         for (int i = 0; i < 10; i++) {
-            int pos1X = (int) Math.random() * 20;
-            int pos1Y = (int) Math.random() * 20;
-            int pos2X = (int) Math.random() * 20;
-            int pos2Y = (int) Math.random() * 20;
+            int pos1X = (int) (Math.random() * 20);
+            int pos1Y = (int) (Math.random() * 20);
+            int pos2X = (int) (Math.random() * 20);
+            int pos2Y = (int) (Math.random() * 20);
             Position pos1 = new Position(pos1X, pos1Y);
             Position pos2 = new Position(pos2X, pos2Y);
             pos1.subtract(pos2);
@@ -53,9 +52,9 @@ public class ExampleUnitTest {
     @Test
     public void PositionMultFactor() {
         for (int i = 0; i < 10; i++) {
-            int pos1X = (int) Math.random() * 20;
-            int pos1Y = (int) Math.random() * 20;
-            int coeff = (int) Math.random() * 30 - 15;
+            int pos1X = (int) (Math.random() * 20);
+            int pos1Y = (int) (Math.random() * 20);
+            int coeff = (int) (Math.random() * 30 - 15);
             Position pos1 = new Position(pos1X, pos1Y);
             pos1.mult(coeff);
             assertEquals(pos1.getX(), pos1X * coeff);
@@ -66,10 +65,10 @@ public class ExampleUnitTest {
     @Test
     public void PositionMultPosition() {
         for (int i = 0; i < 10; i++) {
-            int pos1X = (int) Math.random() * 20;
-            int pos1Y = (int) Math.random() * 20;
-            int pos2X = (int) Math.random() * 20;
-            int pos2Y = (int) Math.random() * 20;
+            int pos1X = (int) (Math.random() * 20);
+            int pos1Y = (int) (Math.random() * 20);
+            int pos2X = (int) (Math.random() * 20);
+            int pos2Y = (int) (Math.random() * 20);
             Position pos1 = new Position(pos1X, pos1Y);
             Position pos2 = new Position(pos2X, pos2Y);
             pos1.mult(pos2);
