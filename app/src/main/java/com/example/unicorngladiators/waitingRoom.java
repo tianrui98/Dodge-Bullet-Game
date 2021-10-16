87f79ecf48f8b045f5fab067bb05804407db51e4
Ivan Leo <ivanleomk@gmail.com>
2021-10-16 09:58:15 +0800

    Added a waiting room activity

---
+++ app/src/main/java/com/example/unicorngladiators/waitingRoom.java
@@ -0,0 +1,14 @@
+package com.example.unicorngladiators;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class waitingRoom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting_room2);
+    }
+}
