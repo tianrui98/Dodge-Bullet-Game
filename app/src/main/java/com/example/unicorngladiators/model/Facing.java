470131008f5fdc1e76c3f4113e278fc468901f5b
Sewen Thy <sewenthy@gmail.com>
2021-11-05 19:31:36 +0800

    wip

--- app/src/main/java/com/example/unicorngladiators/model/Facing.java
+++ app/src/main/java/com/example/unicorngladiators/model/Facing.java
@@ -1,5 +1,10 @@
 package com.example.unicorngladiators.model;
 
+/**
+ * The enumeration of the Character "facings".
+ * It is used to decide which sides (sprite) of the Character
+ * to render.
+ */
 public enum Facing{
     UP,
     DOWN,
