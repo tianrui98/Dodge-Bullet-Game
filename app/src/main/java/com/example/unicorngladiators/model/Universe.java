627c311ecfa99562e8a762d49f950d3418129a5e
Ivan Leo <ivanleomk@gmail.com>
2021-11-05 20:52:40 +0800

    Fixed up new documentation for Universe

--- app/src/main/java/com/example/unicorngladiators/model/Universe.java
+++ app/src/main/java/com/example/unicorngladiators/model/Universe.java
@@ -16,12 +16,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The type Universe.
+ */
 public class Universe {
     private final Joystick joystick;
     private final List<Bullet> bullets;
     private List<Bullet> currentBullets;
     private int bulletIndex;
     private Princess princess;
+    /**
+     * The Players.
+     */
     public HashMap<String, Unicorn> players;
     private final String TAG = "Universe";
     private int height;
@@ -36,6 +42,19 @@ public class Universe {
     private long period;
 
 
+    /**
+     * Instantiates a new Universe.
+     *
+     * @param players           This maps the player names to their respective Unicorns
+     * @param height            Height of the Screen
+     * @param width             Width of the Screen
+     * @param room              This represents the Room Object
+     * @param currentPlayerUID  the current player uid
+     * @param currentPlayerName the current player name
+     *
+     * Using this constructor we can instantiate an instance of the Universe which helps track and manage
+     * changes to our individual player metadata
+     */
     public Universe(HashMap<String,Unicorn> players, int height,int width, Room room, String currentPlayerUID, String currentPlayerName) {
         this.players = players;
         this.princess = new Princess(new Position(20,20), CharacterState.SPECIAL1, height, width);
@@ -76,24 +95,53 @@ public class Universe {
         this.period = 20;
     }
 
-    //manage princess (npc)
+    /**
+     * Gets princess.
+     *
+     * @return the princess
+     */
     public Princess getPrincess() {
         return this.princess;
     }
 
+    /**
+     * Update princess.
+     *
+     * @param state the state
+     * @param pos   the pos
+     */
     public void updatePrincess(CharacterState state, Position pos) {
         this.princess.setState(state);
         this.princess.setPosition(pos);
     }
 
+    /**
+     * Get player lives int.
+     *
+     * @return the int
+     */
     public int getPlayerLives(){
         return this.players.get(this.currentPlayerName).getLives();
     }
 
+    /**
+     * Valid collision boolean.
+     *
+     * @param x        the x
+     * @param y        the y
+     * @param distance the distance
+     * @return the boolean
+     */
     static public boolean validCollision(Bullet x, Unicorn y, int distance){
         return y.getPosition().getDistance(x.getPosition()) <= distance && !y.getIsInvulnerable();
     }
 
+    /**
+     * Check collision helper.
+     *
+     * @param e The Mapping of the Unicorn to the indiivudal names of the players
+     * @param b The List of bullets which are currently moving around in the Universe
+     */
     static public void checkCollisionHelper(HashMap<String, Unicorn> e,List<Bullet> b){
         for(Map.Entry<String,Unicorn> entry:e.entrySet()){
             Unicorn unicorn = entry.getValue();
@@ -112,11 +160,17 @@ public class Universe {
 
     }
 
+    /**
+     * This checks if a collission is happening amongst any of the individual bullets and players
+     */
     public void checkCollision() {
         Universe.checkCollisionHelper(this.players,this.bullets);
     }
 
-    //manage projectiles on screen
+    /**
+     * Adds the next bullet to the list of bullets currently in the Universe
+     */
+//manage projectiles on screen
     public void addABullet(){
         if (this.bulletIndex >= this.bullets.size())
             return;
@@ -125,85 +179,161 @@ public class Universe {
         this.currentBullets.add(bullet);
     }
 
+    /**
+     * Remove a bullet.
+     */
     public void removeABullet(){
         //Todo: only remove a bullet when it's out of the arena or hit a unicorn
         this.currentBullets.remove(this.currentBullets.size() - 1);
     }
 
+    /**
+     * Update all existing bullet positions.
+     */
     public void updateCurrentBulletPosition(){
         for (Bullet bullet : this.currentBullets) {
             bullet.step();
         }
     }
 
+    /**
+     * Add a peach.
+     */
     public void addAPeach(){
         Projectile peach = new Peach(1.0, this.getPrincess(), width, height);
         this.currentPeaches.add(peach);
     }
 
-    //manage players
+    /**
+     * Add player.
+     *
+     * @param name  Name of Player's Unicorn
+     * @param pos   PLayer's current Position
+     * @param state State => This corresponds to the Direction ENUM we defined earlier
+     */
+//manage players
     public void addPlayer(String name, Position pos, CharacterState state) {
         this.players.put(name, new Unicorn (name, 3, false, pos, state));
         castChanges();
     }
+
+    /**
+     * Gets players hash map.
+     *
+     * @return the players hash map
+     */
     public HashMap<String, Unicorn> getPlayersHashMap() {
         return this.players;
     }
 
-        //chang player position
+    /**
+     * Update player position.
+     *
+     * @param name Name of Player's Unicorn
+     * @param m    Motion : The direction which the player is moving in
+     */
+//chang player position
     public void updatePlayerPosition(String name, Motion m) {
         Unicorn player = this.players.get(name);
         player.walk(m);
         this.players.put(name,player);
-//        Log.d(TAG, "Player position changed");
     }
 
+    /**
+     * Gets bullets.
+     *
+     * @return the bullets
+     */
     public List<Bullet> getBullets() {
         return this.currentBullets;
     }
 
 
+    /**
+     * Gets peaches.
+     *
+     * @return the peaches
+     */
     public List<Projectile> getPeaches() {
         return this.currentPeaches;
     }
 
 
-    //manage callback
+    /**
+     * The interface Callback.
+     */
+//manage callback
     public interface Callback {
+        /**
+         * Universe changed.
+         *
+         * @param u the u
+         */
         void universeChanged(Universe u);
     }
 
-    //manage joystick
+    /**
+     * Gets joystick.
+     *
+     * @return the joystick
+     */
+//manage joystick
     public Joystick getJoystick() {
-        return this.joystick ; }
+        return this.joystick ;
+    }
 
+    /**
+     * Set actuator for joystick.
+     *
+     * @param eventPos the event pos
+     */
     public void setActuatorForJoystick(Position eventPos){
         this.joystick.setActuator(eventPos);
-        }
-    public void setIsPressedForJoystick(boolean pressed) {
-        this.joystick.setIsPressed(pressed); }
+    }
 
+    /**
+     * Sets is pressed for joystick.
+     *
+     * @param pressed Boolean value indicating that joystick is pressed or not
+     */
+    public void setIsPressedForJoystick(boolean pressed) {
+        this.joystick.setIsPressed(pressed);
+    }
+
+
+    /**
+     * Reset actuator for joystick.
+     */
     public void resetActuatorForJoystick() {
         this.joystick.resetActuator(); }
 
     /**
-     * Step tells universe what to update
+     * Step updates the universe every 20 ms
+     * <p>
+     * There are a few things in the universe which are updated with each iteration
+     * 1. Princess Peach's position
+     * 2. The Player's existing Position ( The player can only update his position and no other player's position.
+     * 3. Bullet Amount and Positions
+     * 4. Any existing collisions that a unicorn might have with a bullet
+     * 5. We then update the local state of all the player positions by making a call to firebase
      */
     public void step() {
-        //  Log.d(TAG, ("Elapsed time = " + Long.toString(elapsedTime)));
+
+        // Update Princess Peach's position
         this.princess.spin();
         this.princess.stroll();
 
+        // Update Player's existing position
         this.joystick.update();
         this.players.get(this.currentPlayerName).updatePositionState(this.joystick.getActuatorX(), this.joystick.getActuatorY());
         this.updateCurrentBulletPosition();
         this.fph.updateMove(this.players.get(this.currentPlayerName).getPosition().shortString());
 
+        // We Add a bullet every 20 calls to step ~ 400 MS
         if(this.steps % this.period == 0){
             this.addABullet();
         }
         this.steps += 1;
-        // TODO add bullets
 
 
         // TODO add peach at random times
@@ -232,25 +362,22 @@ public class Universe {
 
     private Callback callback = null;
 
+    /**
+     * Sets call back.
+     *
+     * @param c the c
+     */
     public void setCallBack(Callback c) {
         callback = c;
     }
 
-    public void addCallBack(Callback c) {
-        this.callback = c;
-    }
-
+    /**
+     * Cast changes.
+     */
     protected void castChanges() {
         if (callback != null) {
             callback.universeChanged(this);
         }
     }
 
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWidth() {
-        return width;
-    }
 }
